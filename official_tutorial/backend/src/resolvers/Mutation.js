196a04793cb39aeeea98830ba482ece4bad79d36
Ryomasao <konoemario@gmail.com>
2020-03-16 19:15:06 +0900

    vote実装

--- official_tutorial/backend/src/resolvers/Mutation.js
+++ official_tutorial/backend/src/resolvers/Mutation.js
@@ -1,83 +1,81 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { APP_SECRET, getUserId } = require('../utils')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { APP_SECRET, getUserId } = require("../utils");
 
 const signup = async (parent, args, context, info) => {
-  const password = await bcrypt.hash(args.password, 10)
-  const user = await context.prisma.createUser({ ...args, password })
-  const token = jwt.sign({ userId: user.id}, APP_SECRET)
-  return { token, user }
-}
+  const password = await bcrypt.hash(args.password, 10);
+  const user = await context.prisma.createUser({ ...args, password });
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+  return { token, user };
+};
 
 const login = async (parent, args, context, info) => {
-  const user = await context.prisma.user({ email: args.email })
-  if(!user) {
-    throw new Error('No such user found')
+  const user = await context.prisma.user({ email: args.email });
+  if (!user) {
+    throw new Error("No such user found");
   }
 
-  const valid = await bcrypt.compare(args.password, user.password)
-  if(!valid) {
-    throw new Error('Invalid password')
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error("Invalid password");
   }
 
-  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
   return {
     token,
     user
-  }
-}
-
+  };
+};
 
 const post = (parent, args, context, info) => {
- const userId = getUserId(context)
+  const userId = getUserId(context);
 
- // Promise返すんだけど、どのレイヤでawaitしてんだろ
- return context.prisma.createLink({
-   url: args.url,
-   description: args.description,
-   // connectっていうのもprismaの仕様っぽい
-   // https://www.prisma.io/docs/prisma-client/basic-data-access/writing-data-JAVASCRIPT-rsc6/
-   // Schemaで、とあるtypeが別typeを包括している場合、RDB的には、2レコード分更新してるようなイメージになってるんだと思う。
-   postedBy: { connect: { id: userId }}
- })
-}
+  // Promise返すんだけど、どのレイヤでawaitしてんだろ
+  return context.prisma.createLink({
+    url: args.url,
+    description: args.description,
+    // connectっていうのもprismaの仕様っぽい
+    // https://www.prisma.io/docs/prisma-client/basic-data-access/writing-data-JAVASCRIPT-rsc6/
+    // Schemaで、とあるtypeが別typeを包括している場合、RDB的には、2レコード分更新してるようなイメージになってるんだと思う。
+    postedBy: { connect: { id: userId } }
+  });
+};
 
 // https://www.prisma.io/docs/prisma-client/basic-data-access/reading-data-JAVASCRIPT-rsc2/
 // https://www.prisma.io/docs/prisma-client/basic-data-access/writing-data-JAVASCRIPT-rsc6/
 const updateLink = (parent, args, context, info) => {
-  const { id , ...updatedData } = args
+  const { id, ...updatedData } = args;
   return context.prisma.updateLink({
     data: {
-     ...updatedData
+      ...updatedData
     },
     where: { id }
-  })
-}
+  });
+};
 
 const deleteLink = (parent, args, context, info) => {
-  return context.prisma.deleteLink({id: args.id})
-}
+  return context.prisma.deleteLink({ id: args.id });
+};
 
 const vote = async (parent, args, context, info) => {
-  const userId = getUserId(context)
+  const userId = getUserId(context);
 
   const voteExists = await context.prisma.$exists.vote({
     user: { id: userId },
     link: { id: args.linkId }
-  })
+  });
 
+  // TODO vote取り消し機能がほしい
   if (voteExists) {
-    throw new Error(`Already voted for link:${args.linkId}`)
+    throw new Error(`Already voted for link:${args.linkId}`);
   }
 
   return context.prisma.createVote({
-    user: { connect: { id: userId }},
-    link: { connect: { id: args.linkId }}
-  })
-}
-
-
+    user: { connect: { id: userId } },
+    link: { connect: { id: args.linkId } }
+  });
+};
 
 module.exports = {
   signup,
@@ -85,5 +83,5 @@ module.exports = {
   post,
   updateLink,
   deleteLink,
-  vote,
-}
+  vote
+};
--- official_tutorial/front/src/components/Link.js
+++ official_tutorial/front/src/components/Link.js
@@ -1,20 +1,42 @@
 import React from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
 
 import { AUTH_TOKEN } from "../constants";
+import { timeDifferenceForDate } from "../utils";
 
 const Link = props => {
   const authToken = localStorage.getItem(AUTH_TOKEN);
 
-  const handleVote = () => {};
+  const VOTE_MUTATION = gql`
+    mutation VoteMutation($linkId: ID!) {
+      vote(linkId: $linkId) {
+        id
+      }
+    }
+  `;
 
   return (
     <div className="flex mt2 items-start">
       <div className="flex items-center">
         <span className="gray">{props.index + 1}.</span>
         {authToken && (
-          <div className="ml1 gray f11" onClick={handleVote}>
-            ▲
-          </div>
+          <Mutation
+            mutation={VOTE_MUTATION}
+            variables={{ linkId: props.link.id }}
+          >
+            {voteMutation => {
+              return (
+                <div
+                  className="ml1 gray f11"
+                  style={{ cursor: "pointer" }}
+                  onClick={voteMutation}
+                >
+                  ▲
+                </div>
+              );
+            }}
+          </Mutation>
         )}
       </div>
       <div className="ml1">
@@ -24,9 +46,8 @@ const Link = props => {
         </div>
         <div className="f6 lh-copy gray">
           {props.link.votes.length} votes | by{`user name`}
-          {props.link.postedBy ? props.link.postedBy.name : "Unknown"}
-          {""}
-          {/** TODO timediffer */}
+          {props.link.postedBy ? props.link.postedBy.name : "Unknown"}{" "}
+          {timeDifferenceForDate(props.link.createdAt)}
         </div>
       </div>
     </div>
