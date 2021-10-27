b95114ed9e6148fc75d2fa366dfe46a3d9f2ea70
Ryomasao <konoemario@gmail.com>
2021-10-27 10:37:07 +0900

    backendにsubscription追加

--- oreilly_app/backend/index.js
+++ oreilly_app/backend/index.js
@@ -1,5 +1,6 @@
+const { createServer } = require("http");
 const { readFileSync } = require("fs");
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer, PubSub } = require("apollo-server-express");
 const express = require("express");
 const expressPlayground =
   require("graphql-playground-middleware-express").default;
@@ -11,22 +12,27 @@ const resolvers = require("./resolvers");
 const typeDefs = readFileSync("./typeDefs.graphql", "utf8");
 
 async function start() {
-  const MONDO_DB = process.env.DB_HOST
-  const client = await MongoClient.connect(MONDO_DB, { useNewUrlParser: true })
-  const db = client.db()
+  const MONDO_DB = process.env.DB_HOST;
+  const client = await MongoClient.connect(MONDO_DB, { useNewUrlParser: true });
+  const db = client.db();
 
   const app = express();
+  const pubsub = new PubSub();
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: async ({ req }) => {
-      const githubToken = req.headers.authorization
-      const currentUser = await db.collection('users').findOne({ githubToken })
-      return { db, currentUser }
-    }
+    context: async ({ req, connection }) => {
+      const githubToken = req
+        ? req.headers.authorization
+        : connection.context.Authorization;
+      const currentUser = await db.collection("users").findOne({ githubToken });
+      return { db, currentUser, pubsub };
+    },
   });
 
   server.applyMiddleware({ app });
+  const httpServer = createServer(app);
+  server.installSubscriptionHandlers(httpServer);
 
   app.get("/", (req, res) => {
     return res.end("Welcome to the PhotoShare API");
@@ -37,7 +43,7 @@ async function start() {
   // /graphqlにブラウザでアクセスしてもplaygroundと同じ画面にみえる。playground必要なのかしら。
   app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
 
-  app.listen({ port: 4000 }, () => {
+  httpServer.listen({ port: 4000 }, () => {
     console.log(`GraphGL server running on ${server.graphqlPath}`);
   });
 }
--- oreilly_app/backend/resolvers.js
+++ oreilly_app/backend/resolvers.js
@@ -1,138 +1,183 @@
-const { GraphQLScalarType } = require('graphql')
-const { authorizeWithGithub, getFakeUsers } =  require('./lib')
+const { GraphQLScalarType } = require("graphql");
+const { authorizeWithGithub, getFakeUsers } = require("./lib");
 
 const users = [
-	{githubLogin: 'u1', name: 'user1'},
-	{githubLogin: 'u2', name: 'user2'},
-	{githubLogin: 'u3', name: 'user3'}
-]
+  { githubLogin: "u1", name: "user1" },
+  { githubLogin: "u2", name: "user2" },
+  { githubLogin: "u3", name: "user3" },
+];
 
 const photos = [
-	{id: 100, name: 'p1', description:'this is p1', category: "SELFIE", githubUser: 'u1', created: '2021/01/01'},
-	{id: 101, name: 'p2', description:'this is p2', category: "PORTRAIT", githubUser: 'u2', created: '2021/01/02'},
-	{id: 102, name: 'p3', description:'this is p3', category: "PORTRAIT", githubUser: 'u2', created: '2021/01/03'},
-]
+  {
+    id: 100,
+    name: "p1",
+    description: "this is p1",
+    category: "SELFIE",
+    githubUser: "u1",
+    created: "2021/01/01",
+  },
+  {
+    id: 101,
+    name: "p2",
+    description: "this is p2",
+    category: "PORTRAIT",
+    githubUser: "u2",
+    created: "2021/01/02",
+  },
+  {
+    id: 102,
+    name: "p3",
+    description: "this is p3",
+    category: "PORTRAIT",
+    githubUser: "u2",
+    created: "2021/01/03",
+  },
+];
 
 const tags = [
-	{photoId: 100, userId: 'u1'},
-	{photoId: 101, userId: 'u1'},
-	{photoId: 102, userId: 'u2'},
-]
+  { photoId: 100, userId: "u1" },
+  { photoId: 101, userId: "u1" },
+  { photoId: 102, userId: "u2" },
+];
 
 module.exports = resolvers = {
-	Query: {
-		totalPhotos: (parent, args, { db }) => {
-			return db.collection('photos').estimatedDocumentCount()
-		},
-		allPhotos: (parent, args, { db }) => db.collection('photos').find().toArray(),
-		totalUsers: (parent, args, { db }) => db.collection('users').estimatedDocumentCount(),
-		allUsers: (parent, args, { db }) => db.collection('users').find().toArray(),
-		me: (parent, args, { currentUser }) => currentUser
-	},
-	Mutation: {
-		// Mutationのresolverの第1引数って親への参照になるんだっけ？
-		async postPhoto(parent, args, context) {
-			if(!context.currentUser) {
-				throw new Error('Unauthorized')
-			}
+  Query: {
+    totalPhotos: (parent, args, { db }) => {
+      return db.collection("photos").estimatedDocumentCount();
+    },
+    allPhotos: (parent, args, { db }) =>
+      db.collection("photos").find().toArray(),
+    totalUsers: (parent, args, { db }) =>
+      db.collection("users").estimatedDocumentCount(),
+    allUsers: (parent, args, { db }) => db.collection("users").find().toArray(),
+    me: (parent, args, { currentUser }) => currentUser,
+  },
+  Mutation: {
+    // Mutationのresolverの第1引数って親への参照になるんだっけ？
+    async postPhoto(parent, args, context) {
+      if (!context.currentUser) {
+        throw new Error("Unauthorized");
+      }
 
-			const newPhoto = {
-				...args.input,
-				userId: context.currentUser.githubLogin,
-				created: new Date()
-			}
+      const newPhoto = {
+        ...args.input,
+        userId: context.currentUser.githubLogin,
+        created: new Date(),
+      };
 
-			const result  = await context.db.collection('photos').insertOne(newPhoto)
-			newPhoto.id = result.insertedId
+      const result = await context.db.collection("photos").insertOne(newPhoto);
+      newPhoto.id = result.insertedId;
 
-			return newPhoto
-		},
-		async githubAuth(parent, { code }, { db }) {
-			const {
-				message,
-				access_token,
-				avatar_url,
-				login,
-				name
-			} = await authorizeWithGithub({
-				client_id: process.env.CLIENT_ID,
-				client_secret: process.env.CLIENT_SECRET,
-				code
-			})
+      context.pubsub.publish("photo-added", { newPhoto });
 
-			if(message) {
-				throw new Error(message)
-			}
+      return newPhoto;
+    },
+    async githubAuth(parent, { code }, { db }) {
+      const { message, access_token, avatar_url, login, name } =
+        await authorizeWithGithub({
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          code,
+        });
 
-			const latestUserInfo = {
-				name,
-				githubLogin: login,
-				githubToken: access_token,
-				avatar: avatar_url
-			}
+      if (message) {
+        throw new Error(message);
+      }
 
-			await db.collection('users')
-				.replaceOne({githubLogin: login}, latestUserInfo, {upsert: true})
+      const latestUserInfo = {
+        name,
+        githubLogin: login,
+        githubToken: access_token,
+        avatar: avatar_url,
+      };
 
-		return { user: latestUserInfo, token: access_token }
-		},
-		addFakeUsers: async (root, { count }, { db }) => {
-			const users = await getFakeUsers(count)
-			await db.collection('users').insertMany(users)
-			return users
-		},
-		fakeUserAuth: async (root, {githubLogin}, { db } ) => {
-			const user = await db.collection('users')
-				.findOne({githubLogin})
+      const result = await db
+        .collection("users")
+        .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true });
+			
+			result.upserted && pubsub.publish('user-added', { newUser: latestUserInfo })
 
-			if(!user) {
-				throw new Error(`Cannot find user with githubLogin ${githubLogin}`)
-			}
+      return { user: latestUserInfo, token: access_token };
+    },
+    addFakeUsers: async (root, { count }, { db, pubsub }) => {
+      const users = await getFakeUsers(count);
 
-			return {
-				token: user.githubToken,
-				user
-			}
-		}
-	},
-	// トリビアルリゾルバなるもの(ルートのresolverに定義されるオブジェクト)
-	// 
-	// ここでは、永続化する必要がなく都度導出の値URLを返却するために利用する
-	Photo: {
-		// parentはPhotoを表す
-		id: (parent) =>  
-		// DBから取得したオブジェクトはautoIncrementによりカラム名が_idになってる
-		parent.id || parent._id,
-		url: (parent) => `http://foo.bar/img/${parent._id}.jpg`,
-		postedBy: (parent, args, { db }) => {
-			return db.collection('users').findOne({ githubLogin: parent.userId })
-		},
-		taggedUsers: (parent) => {
-			const targetPhotoTags  =  tags.filter(t => t.photoId === parent.id)
-			return targetPhotoTags.map(({ userId }) => users.find(u => u.githubLogin === userId))
-		}
-	},
-	User: {
-		postedPhotos: (parent) => {
-			return photos.filter(p => p.githubUser === parent.githubLogin)
-		},
-		inPhotos: (parent) => {
-			const targetUserTags  =  tags.filter(t => t.userId === parent.githubLogin)
-			return targetUserTags.map(({photoId}) => {photos.find(p => p.id === photoId)})
-		}
-	},
-	DateTime: new GraphQLScalarType({
-		name: 'DateTime',
-		description: 'a valid date time value',
-		// レスポンスで返す際の型変換を実装する必要がある
-		// シリアライズがしっくりこない。stringifyがしっくりくる
-		serialize: value => new Date(value).toISOString(),
-		// リクエスト値をresolverにわたす前に整形する実装
-		// クエリ変数で利用される場合はこっち
-		// eg) query recentPost($after: DateTime)
-		parseValue: value => new Date(value),
-		// クエリに直接文字列を書く場合はこっち
-		// eg) query recentPost(after: '2021/01/01')
-		parseLiteral: ast => ast.value
-	})
-}
+      users.forEach((user) => {
+        pubsub.publish("user-added", { newUser: user });
+      });
+
+      await db.collection("users").insertMany(users);
+      return users;
+    },
+    fakeUserAuth: async (root, { githubLogin }, { db }) => {
+      const user = await db.collection("users").findOne({ githubLogin });
+
+      if (!user) {
+        throw new Error(`Cannot find user with githubLogin ${githubLogin}`);
+      }
+
+      return {
+        token: user.githubToken,
+        user,
+      };
+    },
+  },
+  Subscription: {
+    newPhoto: {
+      subscribe: (parent, args, { pubsub }) => {
+        return pubsub.asyncIterator("photo-added");
+      },
+    },
+    newUser: {
+      subscribe: (parent, args, { pubsub }) => {
+        return pubsub.asyncIterator("user-added");
+      },
+    },
+  },
+  // トリビアルリゾルバなるもの(ルートのresolverに定義されるオブジェクト)
+  //
+  // ここでは、永続化する必要がなく都度導出の値URLを返却するために利用する
+  Photo: {
+    // parentはPhotoを表す
+    id: (parent) =>
+      // DBから取得したオブジェクトはautoIncrementによりカラム名が_idになってる
+      parent.id || parent._id,
+    url: (parent) => `http://foo.bar/img/${parent._id}.jpg`,
+    postedBy: (parent, args, { db }) => {
+      return db.collection("users").findOne({ githubLogin: parent.userId });
+    },
+    taggedUsers: (parent) => {
+      const targetPhotoTags = tags.filter((t) => t.photoId === parent.id);
+      return targetPhotoTags.map(({ userId }) =>
+        users.find((u) => u.githubLogin === userId)
+      );
+    },
+  },
+  User: {
+    postedPhotos: (parent) => {
+      return photos.filter((p) => p.githubUser === parent.githubLogin);
+    },
+    inPhotos: (parent) => {
+      const targetUserTags = tags.filter(
+        (t) => t.userId === parent.githubLogin
+      );
+      return targetUserTags.map(({ photoId }) => {
+        photos.find((p) => p.id === photoId);
+      });
+    },
+  },
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "a valid date time value",
+    // レスポンスで返す際の型変換を実装する必要がある
+    // シリアライズがしっくりこない。stringifyがしっくりくる
+    serialize: (value) => new Date(value).toISOString(),
+    // リクエスト値をresolverにわたす前に整形する実装
+    // クエリ変数で利用される場合はこっち
+    // eg) query recentPost($after: DateTime)
+    parseValue: (value) => new Date(value),
+    // クエリに直接文字列を書く場合はこっち
+    // eg) query recentPost(after: '2021/01/01')
+    parseLiteral: (ast) => ast.value,
+  }),
+};
